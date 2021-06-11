73c2b163f691d607fb34a14fb3937bb16a59083d
nasir733 <engnasir733@gmail.com>
2021-06-11 14:01:52 +0000

    ready to deploy on heroku

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         }
 }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
