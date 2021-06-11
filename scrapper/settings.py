64e817fb795efa0410c840b6fc3906c18715bef4
nasir733 <engnasir733@gmail.com>
2021-06-11 14:38:56 +0000

    added to heroku

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o=8yqu$y4&3ffl0g)g!v1u)sae-*zl=d@#lz4^61t9o6dkt$#b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = []
 BROKER_POOL_LIMIT = 3
