f2d6c9c99cedfd5ff66e0830397377d831902c28
nasir733 <engnasir733@gmail.com>
2021-06-11 14:08:14 +0000

    a

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-# import dj_database_url
 import os
 
 import django_heroku
@@ -107,7 +106,6 @@ DATABASES = {
         }
 }
 
-# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
