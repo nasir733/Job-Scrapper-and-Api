796f66956f9e70355d56204153cb2545c01a6dc6
nasir733 <engnasir733@gmail.com>
2021-06-11 13:21:37 +0000

    ready to deploy on heroku

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = "scrapper.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # if DEBUG:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
+DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # if not DEBUG:
 #     DATABASES = {
