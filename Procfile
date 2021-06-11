28b6c96f176196a01271c0a1b82837041f908eff
nasir733 <engnasir733@gmail.com>
2021-06-11 13:32:12 +0000

    ready to deploy on heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn scrapper.wsgi
 worker: celery -A scrapper worker -l info --pool=solo
+worker-broker:celery -A scrapper beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- scrapper/settings.py
+++ scrapper/settings.py
@@ -88,24 +88,24 @@ WSGI_APPLICATION = "scrapper.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # if DEBUG:
-DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+# DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 
 # if not DEBUG:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": "webscrapper",
-#             "USER": "postgres",
-#             "PASSWORD": "24242424",
-#             "HOST": "database-1.cgflmpcc3zf6.us-east-2.rds.amazonaws.com",
-#             "PORT": "5432",
-#         }
-# }
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "webscrapper",
+            "USER": "postgres",
+            "PASSWORD": "24242424",
+            "HOST": "database-1.cgflmpcc3zf6.us-east-2.rds.amazonaws.com",
+            "PORT": "5432",
+        }
+}
 
 DATABASES['default'] = dj_database_url.config()
 
